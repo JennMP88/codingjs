1593b01980f279e93f6030321ca91c60ed2757c4
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-07 20:19:59 -0400

    added one more solution on warmup-1

--- warmup-1/posNeg.js
+++ warmup-1/posNeg.js
@@ -6,3 +6,23 @@
 // posNeg(-1, 1, false) → true
 // posNeg(-4, -5, true) → true
 
+function posNeg(a, b, negative){
+  let pos=Math.abs(a) //positive
+  let pos2=Math.abs(b) //positive
+  
+if(pos!==a && pos2===b && negative===false){
+  return true}
+
+  else if(pos!==a && pos2===b && negative===true){
+  return false}
+ 
+ else if(pos===a && pos2!==b && negative===false){
+  return true}
+  
+else if(pos!==a && negative===true){
+  return true
+}
+
+  else return false
+ 
+}
