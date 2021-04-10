074f1df9697f323df1ab6e6117e6a2874856db64
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-10 11:38:34 -0400

    fixed some notes

--- logic-1/nearTen.js
+++ logic-1/nearTen.js
@@ -8,18 +8,18 @@
 // nearTen(19) → true
 
 function nearTen(num){
-  //if num is within 2 of the multiple of 0
-
- //(158) → true where modulo is 8
- //162->where modulo is 2
-
-//if 0=num%10--->two up is remainder 1 or 2 and two down is remainder 7 or 8
-
- if(num%10===1 || num%10===2 ||num%10===8 || num%10===0 || num%10==9){
-  
-   return true}
-
- else return false
+   //if num is within 2 of the multiple of 0
+ 
+  //(158) → true where modulo is 8
+  //162->where modulo is 2
+ 
+//if 0=num%10--->two up is remainder 1 or 2 and two down is remainder or 8 or 9
+ 
+  if(num%10===1 || num%10===2 || num%10===0 ||num%10===8 || num%10==9){
+   
+    return true}
+ 
+  else return false
 }
 
 console.log( nearTen(12)) //true
