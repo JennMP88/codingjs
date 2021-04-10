0359970699208ccb8ac9a04d8081782c4a8e5a00
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-10 11:45:34 -0400

    solved for teen sum

--- logic-1/tennSum.js
+++
@@ -1,8 +0,0 @@
-// Logic-1 -- teenSum
-// Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19.
-
-// Examples
-
-// teenSum(3, 4) → 7
-// teenSum(10, 13) → 19
-// teenSum(13, 2) → 19
