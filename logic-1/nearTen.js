4b37113b08ec997288a47988a37063851c372225
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-10 11:26:48 -0400

    initiated files for next three problems

---
+++ logic-1/nearTen.js
@@ -0,0 +1,16 @@
+// Logic-1 -- nearTen
+// Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+
+// Examples
+
+// nearTen(12) → true
+// nearTen(17) → false
+// nearTen(19) → true
+
+function nearTen(num){
+  
+}
+
+console.log( nearTen(12)) //true
+console.log( nearTen(17))//false
+console.log( nearTen(19))//true
