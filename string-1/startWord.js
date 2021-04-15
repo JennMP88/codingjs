bd1849005d71c42730a310f25e200898feb05c09
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-15 08:06:33 -0400

    solved for startWord

--- string-1/startWord.js
+++ string-1/startWord.js
@@ -10,19 +10,32 @@
 // startWord('hippo', 'i') → h
 
 function startWord(str, word){
-  if(word.length>=2){
-    first=word.indexOf(word[0])
-    console.log(first,"1")
-    last=word.length
-    console.log(last,"2")
-    last2=str.substr(first,last)
-    console.log(last2,"3")
-    return last2
+   //if word is in front of str=matches, word[0]!==str[0]
+        //get size of word and see if the front matches. 
+        //the front=str(1) because the str[0] doesn't matter,size of word length 
+  //----if match return the front of the str where 
+  //----if not matching return ""
+
+  //takes length of word and returns a number
+  let wordS=word.length
+  // console.log(wordS,"cut")
+
+  //returns the first few chars in str according to the length of word
+  let cut=str.slice(0,wordS)
+  // console.log(cut,"cut") //hi, hip, h
+
+  // since the first char doesnt have to be the same, takes the rest of the STR up to the length of word
+  let newStr=str.slice(1,wordS)
+  // console.log(newStr,"cut") //if empty and first letter matches return first letter
+
+//  in order to see if it matches the rest of word
+  let cutword=word.slice(1)
+//   console.log(cutword,"cut")
+  
+  if(str===word || newStr===cutword){
+    return cut
   }
-    else if (word<=1){
-      let secondWord=word.indexOf(word)
-      return str.substr(secondWord) 
-    }
+  else return ""
 }
 console.log(startWord('hippo', 'hi'))//hi
 console.log(startWord('hippo', 'xip'))//hip
