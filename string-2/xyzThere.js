be44e7ee19eb7b3c5ddc8f53a27c5583320d5596
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-16 07:29:47 -0400

    initiated xyz ffile

---
+++ string-2/xyzThere.js
@@ -0,0 +1,14 @@
+// String-2 -- xyzThere
+// Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+// Examples
+
+// xyzThere('abcxyz') → true
+// xyzThere('abc.xyz') → false
+// xyzThere('xyz.abc') → true
+function xyzThere(str){
+  
+}
+console.log(xyzThere('abcxyz'))//true
+console.log(xyzThere('abc.xyz'))//false
+console.log(xyzThere('xyz.abc'))//true
