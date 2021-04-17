3a2c05e2c55da6b288936e6426f21e666b3b1372
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-17 16:21:49 -0400

    solved for repeat End

---
+++ string-2/mixString.js
@@ -0,0 +1,15 @@
+// String-2 -- mixString
+// Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+
+// Examples
+
+// mixString('abc', 'xyz') → axbycz
+// mixString('Hi', 'There') → HTihere
+// mixString('xxxx', 'There') → xTxhxexre
+
+function mixString(a, b){
+
+}
+console.log(mixString('abc', 'xyz'))  //→ axbycz
+console.log(mixString('Hi', 'There'))//→ HTihere
+console.log(mixString('xxxx', 'There')) // → xTxhxexre
