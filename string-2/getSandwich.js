c7436bbc77a491bdebc98298bef15fb8e5e90943
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-20 18:19:09 -0400

    solved for repeatSeperator

---
+++ string-2/getSandwich.js
@@ -0,0 +1,31 @@
+// String-2 -- getSandwich
+// A sandwich is two pieces of bread with something in between. 
+//Return the string that is between the first and last appearance of "bread" in the given string,
+// or return the empty string "" if there are not two pieces of bread.
+
+// Examples
+
+// getSandwich('breadjambread') → jam
+// getSandwich('xxbreadjambreadyy') → jam
+// getSandwich('xxbreadyy') →
+
+function getSandwich (str){
+  let string=""
+  let word="bread"
+  let wordLengthIdx=word.length-1 //idx 4
+  let arr=[]
+
+  for(let i=0;i<str.length;i++){
+    if(str[i]==="b"){
+     arr.push(i)
+    }
+  }
+  for(let j=0;j<arr.length;j++){
+     //arr[0,8], [1,9,14]
+    
+  }
+
+
+}
+console.log(getSandwich('breadjambread'))//→ jam
+console.log(getSandwich('xbreadjambreadbryy'))//→ jam
