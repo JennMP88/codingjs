6b0ad8485ea557353b90ff76e0efcd4a63201126
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-24 10:13:49 -0400

    wip zipzap

---
+++ string-2/zipZap.js
@@ -0,0 +1,13 @@
+// String-2 -- zipZap
+// Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+// Examples
+
+// zipZap('zipXzap') → zpXzp
+// zipZap('zopzop') → zpzp
+// zipZap('zzzopzop') → zzzpzp
+
+function zipZap(str){
+  
+}
+console.log(zipZap('zipXzap')) //zpXzp
