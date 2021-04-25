bc5e1f771b6d68dcb19baa11b3d1a50906d4fc9a
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-25 12:48:58 -0400

    solved for lessby10

--- logic-1/inOrder.js
+++ logic-1/inOrder.js
@@ -8,28 +8,24 @@
 // inOrder(1, 2, 1, false) → false
 // inOrder(1, 1, 2, true) → true
 
-function inOrder(a, b, c, bOk){
-
-  if (bOk) {
-    return c > b;
+  function inOrder(a, b, c, bOk){
+    if(b>a && c>b){
+      return true
+    }
+    //where a and b does not need to be compared
+    if(c>b && bOk===true){
+      return true}
+    return false
+   
   }
-  return b > a && c > b;
-  // if(a<b<c && bOk===false){
-  //   if(a>b && b<c && bOk===false){return false}
-  //   return true
-  // }
-  // if(a<b && a===c && bOk==false){return false}
-
-  // if(a===b && b<c && bOk===true){return true}
-  // if(a>b && b<c && bOk===true){return true}
-  // if(a<b && b<c && bOk===true){return true}
-  
-  // if(a<b && b>c && bOk===true){return false}
-  // if(a>b && b>c &&bOk===true){return false}
-  // if(a>b && b===c && bOk===true){return false}
    
 
-}
+
 console.log(inOrder(1, 2, 4, false)) //true
 console.log(inOrder(1, 2, 1, false)) //false
 console.log(inOrder(1, 1, 2, true)) //true
+
+// if (bOk) {
+//   return c > b;
+// }
+// return b > a && c > b;
