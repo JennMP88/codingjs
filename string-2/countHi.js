7f53fc910575d9fe57744b94d3709bb1cf65635a
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-26 19:41:29 -0400

    cleaned code with using slice

--- string-2/countHi.js
+++ string-2/countHi.js
@@ -7,35 +7,46 @@
 // countHi('ABChi hi') → 2
 // countHi('hihi') → 2
 
-
-
 function countHi(str){
-
-  let word="hi"
-  
-  let count=0
-  //look through the string and see if current letter=h
-  // and if current+1 is i
-  // if yes, count ++
-  
+  let count=0;
   for(let i=0;i<str.length;i++){
-    let current=str[i]
-    //  console.log(current,"c")
-  
-    let next=str[i+1]
-  
-    // console.log(current, next,"n")
-  
-    if(current === "h" && next === "i"){
+    let string=str.slice(i,i+2)
+
+    if(string==="hi"){
+      // console.log((str[i+2]))
       count++
     }
-    // return count
-    
   }
-   
-  return count;
+   return count
+ }
+
+// function countHi(str){
+
+//   let word="hi"
   
-  }
+//   let count=0
+//   //look through the string and see if current letter=h
+//   // and if current+1 is i
+//   // if yes, count ++
+  
+//   for(let i=0;i<str.length;i++){
+//     let current=str[i]
+//     //  console.log(current,"c")
+  
+//     let next=str[i+1]
+  
+//     // console.log(current, next,"n")
+  
+//     if(current === "h" && next === "i"){
+//       count++
+//     }
+//     // return count
+    
+//   }
+   
+//   return count;
+  
+//   }
   console.log(countHi('abc hi ho')) // → 1
   console.log(countHi('ABChi hi')) // → 2
   console.log(countHi('hihi')) // → 2
