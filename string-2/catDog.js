d6f078cacba4a497b37833c76775cd44f642ddb8
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-26 19:55:11 -0400

    cleaned up code for catDog problem

--- string-2/catDog.js
+++ string-2/catDog.js
@@ -8,36 +8,54 @@
 // catDog('1cat1cadodog') → true
 
 function catDog(str){
-  let word="cat"
-  let word2="dog"
- 
- let countCat=0;
- let countDog=0;
-
- //is word in the string?
- //if yes then count how many times each word is in str
- //if word count matches return true
-
- //if i find the first letter of word in str and count 3 places and it matches after count ++
- //then take that same for word2 countDog++
- //if the nums match true
-
- for(let i=0;i<str.length;i++){
-   if(str[i]==="c" && str[i+1]==="a" && str[i+2]==="t"){
-     countCat++
+  let catNum=0
+  let dogNum=0
+  
+   for(let i=0;i<str.length;i++){
+     let cat=str.slice(i,i+3)
+     let dog=str.slice(i,i+3)
+     
+     if(cat==="cat"){
+        catNum++}
+     
+     if(dog==="dog"){
+        dogNum++}
    }
-  if(str[i]==="d" && str[i+1]==="o" && str[i+2]==="g"){
-     countDog++
-   }
-
+   if(catNum===dogNum){return true}
+   return false
  }
 
-if(countCat===countDog){
-  return true
-}
-return false
+// function catDog(str){
+//   let word="cat"
+//   let word2="dog"
+ 
+//  let countCat=0;
+//  let countDog=0;
 
-}
+//  //is word in the string?
+//  //if yes then count how many times each word is in str
+//  //if word count matches return true
+
+//  //if i find the first letter of word in str and count 3 places and it matches after count ++
+//  //then take that same for word2 countDog++
+//  //if the nums match true
+
+//  for(let i=0;i<str.length;i++){
+//    if(str[i]==="c" && str[i+1]==="a" && str[i+2]==="t"){
+//      countCat++
+//    }
+//   if(str[i]==="d" && str[i+1]==="o" && str[i+2]==="g"){
+//      countDog++
+//    }
+
+//  }
+
+// if(countCat===countDog){
+//   return true
+// }
+// return false
+
+// }
 console.log(catDog('catdogcat'))// → true
 console.log(catDog('catcat'))// → false
 console.log(catDog('1cat1cadodog'))//→ true
