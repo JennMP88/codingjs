419d8fb80fd9d5eafe949a275012100a5506b3cf
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-28 17:57:57 -0400

    initialized folders for next two problems in strings-3

---
+++ string-3/gHappy.js
@@ -0,0 +1,14 @@
+// We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. 
+//Return true if all the g's in the given string are happy.
+
+// Examples
+
+// gHappy('xxggxx') → true
+// gHappy('xxgxx') → false
+// gHappy('xxggyygxx') → false
+function gHappy(str){
+ 
+}
+console.log(gHappy('xxggxx'))// → true
+console.log(gHappy('xxgxx')) //→ false
+console.log(gHappy('xxggyygxx') //→ false
