f658c85868d245a87314bc4e5462a738d67ba3a4
Jennifer Perez <jenniferperez@pursuit.org>
2021-04-30 20:10:59 -0400

    solution for withoutString

--- string-3/withoutString.js
+++ string-3/withoutString.js
@@ -7,8 +7,46 @@
 // withoutString('Hello there', 'x') → Hello there
 
 function withoutString(base, remove){
-  
+
+  // loop through string base 
+  // find where string is equal to remove
+  // if string at that idx is remove move to next i
+  // and copy next i and check that next ietc equal to remove
+  //   if it's not then copy that i to string
+ let len=remove.length
+ let string=""
+
+ for(let i=0; i<base.length;i++){
+   let sliced=base.slice(i,len+i)
+
+    if(sliced.toLowerCase()===remove.toLowerCase(){
+      i+=len-1
+    }
+   else {
+     string+=base[i]
+   }
+
+ }
+ return string
+
 }
+​
+// or with a while loop
+​
+// function withoutString(base, remove){
+//   let res = ""
+//   let i = 0;
+//   while (i < base.length) {
+//     let substr = base.slice(i, i + remove.length)
+//     if (substr.toLowerCase() === remove.toLowerCase()) {
+//       i += substr.length
+//     } else {
+//       res += base[i]
+//       i++
+//     }
+//   }
+//   return res
+// }
 console.log(withoutString('Hello there', 'llo') )// → He there
 console.log(withoutString('Hello there', 'e'))// → Hllo thr
 console.log(withoutString('Hello there', 'x'))// → Hello there
