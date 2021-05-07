a94d9a13e52532e69693a0440b1f3806e821f8f4
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-07 07:30:28 -0400

    solved for maxEnds

--- array-1/maxEnds.js
+++ array-1/maxEnds.js
@@ -7,15 +7,18 @@
 // maxEnd3([11, 5, 9]) → 11,11,11
 // maxEnd3([2, 11, 3]) → 3,3,3
 function maxEnd3(nums){
-  let arr=[]
-
-    if (nums[nums.length-1]>nums[0]){
-        nums[0]===3
-        nums[1]===3
-  } 
-     if (nums[nums.length-1]<nums[0]){
-        nums[1]===3
-        nums[nums.length-1]===3
-  } 
-  return nums
+  if(nums[2]==nums[0]){
+    nums[1]=nums[2]
+  }
+  if (nums[2]>nums[0]){
+      nums[0]=nums[2]
+      nums[1]=nums[2]
+} 
+   else if (nums[0]>nums[2]){
+      nums[1]=nums[0]
+      nums[2]=nums[0]
+} 
+return nums
 }
+
+console.log(maxEnd3([1, 2, 3]))// → 3,3,3
