8a7018cba88c01fd8dd980c2cd5dcf3ca342b45f
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-06 22:44:55 -0400

    three different solutions to withoutStrings

--- string-3/withoutString.js
+++ string-3/withoutString.js
@@ -6,47 +6,46 @@
 // withoutString('Hello there', 'e') → Hllo thr
 // withoutString('Hello there', 'x') → Hello there
 
-function withoutString(base, remove){
-
-  // loop through string base 
-  // find where string is equal to remove
-  // if string at that idx is remove move to next i
-  // and copy next i and check that next ietc equal to remove
-  //   if it's not then copy that i to string
- let len=remove.length
- let string=""
-
- for(let i=0; i<base.length;i++){
-   let sliced=base.slice(i,len+i)
-
-    if(sliced.toLowerCase()===remove.toLowerCase(){
-      i+=len-1
-    }
-   else {
-     string+=base[i]
-   }
-
- }
- return string
-
-}
-​
-// or with a while loop
-​
 // function withoutString(base, remove){
 //   let res = ""
-//   let i = 0;
-//   while (i < base.length) {
+//   for (let i = 0; i < base.length; i++) {
 //     let substr = base.slice(i, i + remove.length)
-//     if (substr.toLowerCase() === remove.toLowerCase()) {
-//       i += substr.length
+//     if (substr.toLowerCase() === remove.toLowerCase()){
+//       i += substr.length - 1 
 //     } else {
 //       res += base[i]
-//       i++
 //     }
 //   }
 //   return res
 // }
+​
+// or with a while loop
+​
+function withoutString(base, remove){
+  let res = ""
+  let i = 0;
+  while (i < base.length) {
+    let substr = base.slice(i, i + remove.length)
+    if (substr.toLowerCase() === remove.toLowerCase()) {
+      i += substr.length
+    } else {
+      res += base[i]
+      i++
+    }
+  }
+  return res
+} 
+  
+// //new RegEx-regex is an obj.
+// //lets create a new pattern with the remove string
+// //gi search globally and ignore case  
+// let reg=new RegExp(remove,"gi") //creating a pattern that will search for hte string remove globally and ignoring case
+// let modBase=base.replace(reg,"")//pattern,replaces the pattern with the empty string
+
+// return modBase
+
+// }
+
 console.log(withoutString('Hello there', 'llo') )// → He there
 console.log(withoutString('Hello there', 'e'))// → Hllo thr
 console.log(withoutString('Hello there', 'x'))// → Hello there
