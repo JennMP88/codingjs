53e239647d631b19045240f00a36358e969ea597
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-10 17:20:50 -0400

    solved for double23

--- array-1/double23.js
+++ array-1/double23.js
@@ -7,3 +7,13 @@
 // double23([3, 3]) → true
 // double23([2, 3]) → false
 
+function double23(nums){
+  if(nums===[]){return false}
+  for(let i=0;i<nums.length;i++){
+    if(nums[i]===2 && nums[i+1]===2 ||nums[i]===3 && nums[i+1]===3 ){
+      return true
+    }
+    else return false 
+  }
+  return false
+}
