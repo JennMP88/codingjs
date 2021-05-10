fae8d13dba4ae3689fbc87d656397f6dd8107487
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-10 17:56:12 -0400

    solved for frontPiece

---
+++ array-1/maxTriple.js
@@ -0,0 +1,12 @@
+// Array-1 -- maxTriple
+// Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+// Examples
+
+// maxTriple([1, 2, 3]) → 3
+// maxTriple([1, 5, 3]) → 5
+// maxTriple([5, 2, 3]) → 5
+function maxTriple(nums){
+
+}
+console.log( maxTriple([1, 2, 3]))// → 3
