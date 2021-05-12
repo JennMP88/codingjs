e4995bd260c2965b599d4c3f80419d3fdb8ec9a1
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-12 07:34:44 -0400

    solved for maxTriple

--- array-1/maxTriple.js
+++ array-1/maxTriple.js
@@ -7,6 +7,15 @@
 // maxTriple([1, 5, 3]) → 5
 // maxTriple([5, 2, 3]) → 5
 function maxTriple(nums){
-
+  let first=nums[0]
+  let middle=nums[Math.floor(nums.length/2)]
+  let last=nums[nums.length-1]
+  if(first>middle && first>last){
+    return first
+  }
+  if(middle>first && middle>last){
+    return middle
+  }
+  else return last
 }
-console.log( maxTriple([1, 2, 3]))// → 3
+console.log( maxTriple([1, 7, 3, 1, 5]))// → 5
