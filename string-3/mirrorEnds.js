873995f9fb940aa0577abf77695fe4559050c8e0
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-15 11:58:45 -0400

    added description for mirrorEnds

--- string-3/mirrorEnds.js
+++ string-3/mirrorEnds.js
@@ -1,3 +1,12 @@
+// String-3 -- mirrorEnds
+// Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+
+// Examples
+
+// mirrorEnds('abXYZba') → ab
+// mirrorEnds('abca') → a
+// mirrorEnds('aba') → aba
+
 function mirrorEnds(string){
   let bwards=""
   let j=1
