2607fd7ceb33ca71c53b3a10a9c52dae4fe5304c
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-17 18:49:01 -0400

    solved for closefar

---
+++ logic-2/noTeenSum.js
@@ -0,0 +1,21 @@
+//  Logic-2 -- noTeenSum
+//  Given 3 int values, a b c, return their sum. 
+// However, if any of the values is a teen -- in the range 13..19 inclusive -- 
+// then that value counts as 0, except 15 and 16 do not count as a teens. 
+// Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns 
+// that value fixed for the teen rule. In this way, you avoid repeating the teen code 
+// 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as 
+// the main noTeenSum().
+
+
+// noTeenSum(1, 2, 3) → 6
+// noTeenSum(2, 13, 1) → 3
+// noTeenSum(2, 1, 14) → 3
+
+function noTeenSum(a, b, c){
+let arr=[13,14,17,18,19]
+  // param can be between 13,14,17,18,19 not 15,16
+//if a is equal to any number in the array then return the sum of the other nums
+
+console.log(noTeenSum(1,2,3))//6
+console.log(noTeenSum(15,2,3))//5
