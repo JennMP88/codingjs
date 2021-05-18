04a8a1d3790c126994f883564ace5fdfd47330d6
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-18 18:52:36 -0400

    alternative approach to prefixAgain

--- string-2/prefixAgain.js
+++ string-2/prefixAgain.js
@@ -8,25 +8,35 @@
 // prefixAgain('abXYabc', 3) → false
 
 function prefixAgain(str, n){
-  // look at the pref-
-  // define pref by slicing n from the beginning of the str
-  // compare the pref to the rest of the string
-  //if the rest of the str includes pref true 
-  //else false
-  
-  let lower=str.toLowerCase()
-  let pref=lower.slice(0,n)
-  let restStr=lower.slice(n+1)
-  
-  if(str.length-1===1 && str[0]===str[1]){
-    return true
-  }
-  if(restStr.includes(pref)){
-    return true
-  }
-  else return false
+
+  let cut=str.slice(0,n)
+  let rest=str.slice(n)
+ 
+  if(rest.includes(cut)){return true}
+
+  return false
 
 }
+// function prefixAgain(str, n){
+//   // look at the pref-
+//   // define pref by slicing n from the beginning of the str
+//   // compare the pref to the rest of the string
+//   //if the rest of the str includes pref true 
+//   //else false
+  
+//   let lower=str.toLowerCase()
+//   let pref=lower.slice(0,n)
+//   let restStr=lower.slice(n+1)
+  
+//   if(str.length-1===1 && str[0]===str[1]){
+//     return true
+//   }
+//   if(restStr.includes(pref)){
+//     return true
+//   }
+//   else return false
+
+// }
 console.log( prefixAgain('abXYabc', 1)) //true
 console.log(prefixAgain('abXYabc', 2))// true
 console.log(prefixAgain('abXYabc', 3)) //false
