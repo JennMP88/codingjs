5b852fdc2cb2997558651687875f75f1423e321a
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-18 18:46:58 -0400

    minor changes to endOther to make code cleaner

--- string-2/endOther.js
+++ string-2/endOther.js
@@ -6,40 +6,65 @@
 // endOther('Hiabc', 'abc') → true
 // endOther('AbC', 'HiaBc') → true
 // endOther('abc', 'abXabc') → true
-
 function endOther(a, b){
-  let aString= a.toLowerCase()
-  let bString = b.toLowerCase()
+  let aLen=a.length
+  let bLen=b.length
+  
+  
+  a=a.toLowerCase()
+  b=b.toLowerCase()
 
   if(a===b){return true}
 
-  if(aString.length < bString.length){
-    let aIdx= aString.indexOf(aString[0]) //0 
 
-    let aLength=-a.length
-
-    if(bString.includes(aString)) {
-     
-      if(bString.slice(aLength)==aString)
+  if(bLen<aLen){
+    if( a.slice(-bLen)===b){
       return true
     }
-    
   }
 
-  else if (b.length<a.length){
-   let bIdx=bString.indexOf(bString[0])//0
-   let bLength= -b.length
-    
-    if(aString.includes(bString)) {
-    // console.log(aString.slice(bLength), "this")
-    // console.log(aString, "this2")
 
-      if(aString.slice(bLength)==bString)
+  if(aLen<bLen){
+    if(b.slice(-aLen)===a){
       return true
     }
   }
   return false
+
 }
+// function endOther(a, b){
+//   let aString= a.toLowerCase()
+//   let bString = b.toLowerCase()
+
+//   if(a===b){return true}
+
+//   if(aString.length < bString.length){
+//     let aIdx= aString.indexOf(aString[0]) //0 
+
+//     let aLength=-a.length
+
+//     if(bString.includes(aString)) {
+     
+//       if(bString.slice(aLength)==aString)
+//       return true
+//     }
+    
+//   }
+
+//   else if (b.length<a.length){
+//    let bIdx=bString.indexOf(bString[0])//0
+//    let bLength= -b.length
+    
+//     if(aString.includes(bString)) {
+//     // console.log(aString.slice(bLength), "this")
+//     // console.log(aString, "this2")
+
+//       if(aString.slice(bLength)==bString)
+//       return true
+//     }
+//   }
+//   return false
+// }
 console.log(endOther('Hiabc', 'abc')) //→ true
 console.log(endOther('Hiabcd', 'abc')) //→ true
 console.log(endOther('AbC', 'HiaBc')) //→ true
