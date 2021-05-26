cd2e67eba9770f5cb536b969bdf3dc95f17a8d12
Jennifer Perez <jenniferperez@pursuit.org>
2021-05-26 07:52:03 -0400

    solved for eitheir24

--- array-2/eitheir24.js
+++ array-2/eitheir24.js
@@ -7,16 +7,26 @@
 // either24([4, 4, 1]) → true
 // either24([4, 4, 1, 2, 2]) → false
 function either24(nums){
-  let copy=[...nums]
-  let str
-  for(let i=0;i<nums.length;i++){
-    if(nums[i]===2 && nums[i+1]===2){
-     nums.splice(i,2)
-    }
-  }
-  if(copy.length!==nums.length){
-    
-  }
+  // starts at false
+    let checkTwo = 0;
+    let checkFour = 0;
+    for(let i = 0; i < nums.length - 1; i++){
   
-}
+   //if there is a match of 2pairs then change to true   
+      if(nums[i] === 2 && nums[i+1] === 2)
+        checkTwo = 1;
+        // console.log(checkTwo,i, "here")
+  
+   //if there is a match of 4pairs then change to true   
+      if(nums[i] === 4 && nums[i+1] === 4)
+        checkFour= 1;
+        // console.log(checkFour,i, "there")
+    }
+  
+   // console.log(checkTwo, checkFour, "match")
+    //take sum where if 1-true, any other number will be false
+    if(checkTwo+checkFour===1){
+      return true}
+    else return false
+  }
 console.log(either24([4, 4, 1, 2, 2])) //false
