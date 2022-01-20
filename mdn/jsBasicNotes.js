83723c9ac6624aed2e26cab261a4f5c687ff4a52
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-19 20:49:45 -0600

    minor js notes review

---
+++ mdn/jsBasicNotes.js
@@ -0,0 +1,17 @@
+// console.log(.1+.2)
+// console.log(parseFloat("010",10))
+// console.log(typeof NaN)
+
+// console.log(parseInt("hello"))
+
+// let str=["a","b","c"]
+// let mystr=str.toLocaleString();
+// console.log(mystr);
+
+// let name=["j","e,","n","n","i"]
+// let neni=name.slice(0,3)
+// console.log(neni)
+
+let arr=[7,5,15,21]
+arr.sort((a,b)=>b-a)
+console.log(arr)
