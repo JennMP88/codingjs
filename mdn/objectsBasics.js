a0598eb8609bf5eb2886b4dc369f9be1c53e8fad
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-22 16:47:41 -0600

    basic 3 and 4 more on mdn docs

--- mdn/objectsBasics.js
+++ mdn/objectsBasics.js
@@ -34,4 +34,57 @@ bandInfo=`My fav band is ${favBand.name}. They are from ${favBand.nationality} a
           They have released ${favBand.albums[0].name} on ${favBand.albums[0].released}. `
 console.log(bandInfo)
 
-// objects basic 3
+
+// ------------------
+// objects basic 4
+// update task one with new greeting.
+const cat1 = {
+  name : 'Bertie',
+  breed : 'Cymric',
+  color : 'white',
+  greeting: function() {
+    console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+  }
+}
+
+const cat2 = {
+  name : 'Simmons',
+  breed : "exotic shorthair",
+  color : 'grey',
+  greeting: function() {
+    console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+  }
+}
+
+cat1.greeting();
+cat2.greeting();
+
+// -----------------
+// objects basic 4
+// update task one with new greeting.
+// updated to constructor to make it reusable for new instances .
+
+function Cat1 (name){
+  this.name =name;
+  this.breed = 'Cymric';
+  this.color = 'white';
+  this.greeting= function() {
+    console.log(`Hello, said ${this.name} the ${this.breed}.`);
+  }
+}
+// Bertie cat
+const Bertie= new Cat1 ("Bertie");
+Bertie.name;
+Bertie.breed;
+Bertie.color;
+Bertie.greeting;
+
+// cat2
+const Cat2= new Cat1 ("Simmons");
+Cat2.name;
+Cat2.breed="exotic shorthair";
+Cat2.color="yellow";
+Cat2.greeting;
+
+console.log(Bertie.greeting())
+console.log(Cat2.greeting())
---
+++ mdn/practs.js
@@ -0,0 +1,27 @@
+const person = {
+  name: ['Bob', 'Smith'],
+  age: 32,
+  bio: function() {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function() {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  }
+};
+
+person.age = 45;
+person['name']['last'] = 'Cratchit';
+
+
+person['eyes'] = 'hazel';
+person.farewell = function() {
+  console.log('Bye everybody!');
+}
+
+console.log(
+  person
+  // person.name[0],
+  // person.age,
+  // person.bio(),
+  // person.introduceSelf()
+  )
